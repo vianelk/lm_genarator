82409793d7be8f9576e5fa8556875a8712cd6120
Vianel Kong <vianelkong@gmail.com>
2024-06-13 01:59:00 +0200

    pis

--- letterGenetor.py
+++ letterGenetor.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 class LetterGenerator:
     
-    def __init__(self, cover_letter_path : str) -> None:
+    def __init__(self, cover_letter_path : str,skills_entreprises : str) -> None:
         self.cover_letter_path = cover_letter_path
+        self.skills_entreprises = skills_entreprises
         pass
     
     def generate_cover_letter(self, entreprise_name : str, entreprise_contact : pd.DataFrame):
@@ -20,7 +21,10 @@ class LetterGenerator:
                                                   name=rows["NOM"],
                                                   sexe=rows["SEXE"])
 
-            document.save(f"./data/lettres de motivation/{entreprise_name}/lettre de motivation - {rows["NOM"]}.docx")
+            document.save(f"./data/lettres de motivation/{entreprise_name}/ {rows["NOM"]} - lettre de motivation.docx")
+            
+            first_name, _= rows["NOM"].split(" ")
+            self.generate_email(fichier=f"./data/lettres de motivation/{entreprise_name}/email.txt",nom=first_name,skills=self.skills_entreprises[entreprise_name], entreprise=entreprise_name)
         
         
         return ''
@@ -47,3 +51,35 @@ class LetterGenerator:
             if old_text in run.text:
                
                 run.text = run.text.replace(old_text, new_text)
+    
+    def generate_email(self,fichier, nom, entreprise, skills):
+        nouveau_texte = f"""
+CANDIDATURE DATA ENGINEER EN ALTERNANCE CHEZ ORANGE
+        
+Bonjour {nom},
+J'espère que vous allez bien.
+
+Je me permets de vous contacter pour vous proposer ma candidature pour une alternance en data engineering au sein de {entreprise}.
+
+Actuellement étudiant en 4ème année Big data & AI,
+je suis passionné par le domaine du big data et je souhaite développer mes compétences en rejoignant une équipe innovante telle que la vôtre.
+Mes expériences sur les technologies cloud et data ({skills} etc...) me permettront de contribuer efficacement à vos projets.
+Je vous joins mon CV, ma lettre de motivation ainsi qu'une lettre de recommandation pour appuyer ma candidature.
+Je suis disponible immédiatement pour tout entretien afin de vous exposer mes motivations.
+
+Dans l'attente de votre retour, je vous prie d'agréer l'expression de mes salutations distinguées.
+Vianel KONG
+0758334658
+        
+        --------------------------------------------------------------------------------------------------------------
+        
+        
+        """
+
+        try:
+            with open(fichier, 'a') as file:  # Ouvrir le fichier en mode ajout ('a' pour append)
+                file.write(nouveau_texte + '\n')  # Écrire le nouveau texte suivi d'un saut de ligne
+        except FileNotFoundError:
+            print(f"Le fichier {fichier} n'existe pas.")
+        except Exception as e:
+            print(f"Une erreur est survenue: {e}")
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from docx import Document
 from excelReader import ExcelReader
 from letterGenetor import LetterGenerator
 
 def main():
     domain = "TELECOM"
-    entreprises_list =  ["ORANGE"]
+    entreprises_list =  ["ORANGE","SFR","BOUYGUES TELECOM"]
+    entreprises_skills =  {
+        "ORANGE":"CGP, Python, Spark/Scala,Hive, Hadoop",
+        "SFR":"CGP, Python, Hadoop",
+        "BOUYGUES TELECOM":" Python, Hadoop, Hive, Impala, GCP"
+    }
     entreprises_contacts =  ExcelReader(entreprises_list).entreprises_contacts
-    lg = LetterGenerator(f"./data/lettres de motivation/Lettre de motivation - {domain}.docx")
+    lg = LetterGenerator(f"./data/lettres de motivation/Lettre de motivation - {domain}.docx",skills_entreprises = entreprises_skills)
     
     for entreprise in entreprises_list :
         lg.generate_cover_letter(entreprise_name=entreprise,entreprise_contact=entreprises_contacts[entreprise])
